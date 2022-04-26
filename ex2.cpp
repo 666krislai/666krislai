b1871e9b5d75e173f5fe124d018c6c7981ecab9b
666krislai <104436876+666krislai@users.noreply.github.com>
2022-04-26 21:49:39 +0800

    Create ex2.cpp

---
+++ ex2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello World ");
+    
+    cout <<  "hello Krislai ";
+    cout <<  "my height is " << 1 << "m " << 74 << "cm";
+
+    return 0;
+}
