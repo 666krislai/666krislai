00e7ba64ff7d2a232a2ba983c3861cab9e7b92e1
666krislai <104436876+666krislai@users.noreply.github.com>
2022-05-03 21:35:39 +0800

    Create ex4.cpp

---
+++ ex4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  double i ;
+  scanf ("%lf", &i);
+  printf ("%lf\n", i*i*3.14159 );
+  printf ("%lf\n", i*2*3.14159 );
+ 
+
+  
+
+
+  return 0;
+}
